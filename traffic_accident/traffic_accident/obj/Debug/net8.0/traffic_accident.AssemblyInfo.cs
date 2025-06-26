46ba52b66c8a4d7d8280bb503a5494214ebb39ea
Kolesnikov Vladimir <castcutyt48@gmail.com>
2025-06-26 15:32:25 +0400

    проверка программы

--- traffic_accident/traffic_accident/obj/Debug/net8.0/traffic_accident.AssemblyInfo.cs
+++ traffic_accident/traffic_accident/obj/Debug/net8.0/traffic_accident.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("traffic_accident")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8175b68a28d14660f195fcdbcccf4265ed7daef1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c20bf507bfec3a7b5d25b8ee9801619328ca0a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("traffic_accident")]
 [assembly: System.Reflection.AssemblyTitleAttribute("traffic_accident")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
