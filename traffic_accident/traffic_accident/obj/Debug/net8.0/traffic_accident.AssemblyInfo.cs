e7db373e822a9582f7b59500a84dda2c90d58013
Kolesnikov Vladimir <castcutyt48@gmail.com>
2025-06-26 14:43:19 +0400

    123

--- traffic_accident/traffic_accident/obj/Debug/net8.0/traffic_accident.AssemblyInfo.cs
+++ traffic_accident/traffic_accident/obj/Debug/net8.0/traffic_accident.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("traffic_accident")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8175b68a28d14660f195fcdbcccf4265ed7daef1")]
 [assembly: System.Reflection.AssemblyProductAttribute("traffic_accident")]
 [assembly: System.Reflection.AssemblyTitleAttribute("traffic_accident")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
